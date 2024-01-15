142e53362d707e2fa0266d82909da15abdecbe9e
minnasu <minnasu@tencent.com>
2024-01-15 22:22:26 +0800

    refactor:apng

--- src/pages/APNGAnimation/index.tsx
+++ src/pages/APNGAnimation/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import parseAPNG from 'apng-js'
 import styled from 'styled-components'
 import Uploader from '../../components/Uploader'
